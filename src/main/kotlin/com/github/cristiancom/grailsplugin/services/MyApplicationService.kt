538d211f80a07b6f41fef53ba29ac05f7417339e
GitHub Action <action@github.com>
2021-11-01 17:40:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/cristiancom/grailsplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cristiancom/grailsplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cristiancom.grailsplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cristiancom.grailsplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cristiancom/grailsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cristiancom/grailsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cristiancom.grailsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cristiancom.grailsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
